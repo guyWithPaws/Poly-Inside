8a18e03c7f47003276e1d3b8e45a4607e1a67172
goxxxa <g.o.molodtsov@gmail.com>
2024-09-14 21:30:28 +0300

    add home page

--- frontend/lib/entry_points/web_entrypoint.dart
+++ frontend/lib/entry_points/web_entrypoint.dart
@@ -1,1 +1,4 @@
-void runner() => throw UnimplementedError('web');
+import 'package:flutter/widgets.dart';
+import 'package:poly_inside/src/feature/home/home_page.dart';
+
+void runner() => runApp(const HomePage());
