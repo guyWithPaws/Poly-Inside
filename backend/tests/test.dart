c38f7ba913755a8b3e46a03329af614202577cf0
Arseniy <19200508did@gmail.com>
2024-09-15 21:52:34 +0300

    Added Envoy

--- backend/tests/test.dart
+++ backend/tests/test.dart
@@ -1,16 +1,15 @@
-import 'package:grpc/grpc.dart';
-import 'package:shared/shared.dart';
+// import 'package:grpc/grpc.dart';
+// import 'package:shared/shared.dart';
 
-void main(List<String> args) async{
-  final client = SearchServiceClient(
-    ClientChannel(
-      '87.228.18.201',
-      port: 8080,
-      options: const ChannelOptions(
-        credentials: ChannelCredentials.insecure(),
-      ),
-    ),
-  );
-  final data = client.getListProfessor(ListProfessorRequest());
-  data.forEach(print);
-}
+// void main(List<String> args) async{
+//   final client = SearchServiceClient(
+//     ClientChannel(
+//       '87.228.18.201',
+//       port: 8080,
+//       options: const ChannelOptions(
+//         credentials: ChannelCredentials.insecure(),
+//       ),
+//     ),
+//   );
+//   final data = client.getListProfessor(ListProfessorRequest());
+// }
