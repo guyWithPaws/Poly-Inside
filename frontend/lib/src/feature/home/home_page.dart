bcdede5c85288178531dbce119d81917c41842f7
Arseniy <19200508did@gmail.com>
2024-09-15 22:08:58 +0300

    Changed Envoy address

--- frontend/lib/src/feature/home/home_page.dart
+++ frontend/lib/src/feature/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:grpc/grpc.dart';
+import 'package:grpc/grpc_web.dart';
 import 'package:poly_inside/src/common/repository/client.dart';
 import 'package:poly_inside/src/common/repository/client_impl.dart';
 import 'package:shared/shared.dart';
@@ -30,12 +31,8 @@ class _HomePageState extends State<HomePage> {
   void didChangeDependencies() {
     repository = ClientRepositoryImpl(
       client: SearchServiceClient(
-        ClientChannel(
-          '87.228.18.201',
-          port: 8080,
-          options: const ChannelOptions(
-            credentials: ChannelCredentials.insecure(),
-          ),
+        GrpcWebClientChannel.xhr(
+          Uri.parse('http://87.228.18.201:8080'),
         ),
       ),
     );
