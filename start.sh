924e3f0828d81126826c63c1b33b963463908698
Долгий Павел Станиславович <140969933+SkyLorxd@users.noreply.github.com>
2025-01-31 20:26:32 +0300

    Update and rename docker.sh to start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
-sudo apt-get update
+sudo apt update
+
 sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
@@ -16,4 +15,12 @@ sudo apt-get update
 
 sudo apt-get -y  install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo apt update
+
+sudo apt -y install python3
+sudo apt -y install python3-pip
+sudo pip install requests
+sudo pip install lxmlsoup
+
+sudo python3 server.py
 
