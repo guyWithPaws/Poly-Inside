e865249c635bb26afbe3ddb7227c59fc54e87032
goxxxa <g.o.molodtsov@gmail.com>
2025-02-04 01:48:10 +0300

    refactor(backend/shared): Deleted unnecessary duplication "shared" folder

--- backend/shared/lib/src/shared_base.dart
+++
@@ -1,6 +0,0 @@
-// TODO: Put public facing types in this file.
-
-export 'protobuf/service.pb.dart';
-export 'protobuf/service.pbenum.dart';
-export 'protobuf/service.pbgrpc.dart';
-export 'protobuf/service.pbjson.dart';
