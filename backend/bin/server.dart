b4f30aa7b14e54e29ba36bead81871a59766cdc3
goxxxa <g.o.molodtsov@gmail.com>
2025-02-04 04:07:57 +0300

    refactor: Connected server to PostgreSQL database

--- backend/bin/server.dart
+++ backend/bin/server.dart
@@ -3,13 +3,11 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:drift/native.dart';
 import 'package:grpc/grpc.dart';
 import 'package:l/l.dart';
 import 'package:poly_inside_server/database/database.dart';
 import 'package:poly_inside_server/database/provider_impl.dart';
 import 'package:poly_inside_server/logger.dart';
-import 'package:poly_inside_server/parser/parser.dart';
 import 'package:poly_inside_server/service.dart';
 
 Future<void> main() async {
@@ -19,21 +17,25 @@ Future<void> main() async {
       await runZonedGuarded(
         () async {
           final logger = Logger(file: file);
-          
-          final database = AppDatabase(NativeDatabase(File('/db/db.sqlite')));
+
+          final database = AppDatabase();
           final provider = DatabaseProviderImpl(database: database);
 
-          final parser = await Parser.create(provider);
-          unawaited(parser.updateDatabase());
-
-          final server = Server.create(services: [
-            GRPCService(provider: provider),
-          ], interceptors: []);
+          await Future.delayed(const Duration(seconds: 35));
+          final server = Server.create(
+            services: [
+              GRPCService(provider: provider),
+            ],
+            interceptors: [],
+          );
           l.i('[Server]: Server starts!');
+
           await server.serve(port: 9090);
           l.i('[Server]: Server is listening on port ${server.port}');
         },
-        (e, st) {},
+        (e, st) {
+          l.e('[Server]: Uncaught error: $e $st');
+        },
       );
     },
     LogOptions(
