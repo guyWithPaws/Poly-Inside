0d8bdb87abdaa23a57cf5d87a5ea157ab6d5e3c5
goxxxa <g.o.molodtsov@gmail.com>
2025-02-13 22:45:54 +0300

    refactor: Separeted server logic. Cleaned up code

---
+++ backend/server/bin/server.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: unused_local_variable
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:common/common.dart';
+import 'package:grpc/grpc.dart';
+import 'package:l/l.dart';
+
+Future<void> main() async {
+  final file = File('server.log');
+  await l.capture(
+    () async {
+      await runZonedGuarded(
+        () async {
+          final logger = Logger(file: file);
+
+          final database = AppDatabase();
+          await DatabaseProviderImpl.initialize(database);
+          final provider = DatabaseProviderImpl();
+
+          await Future.delayed(const Duration(seconds: 10));
+          final server = Server.create(
+            services: [
+              GRPCService(provider: provider),
+            ],
+            interceptors: [],
+          );
+          l.i('[Server]: Server starts!');
+
+          await server.serve(port: 9090);
+          l.i('[Server]: Server is listening on port ${server.port}');
+        },
+        (e, st) {
+          l.e('[Server]: Uncaught error: $e $st');
+        },
+      );
+    },
+    LogOptions(
+      outputInRelease: true,
+      messageFormatting: (message, logLevel, dateTime) =>
+          // ignore: lines_longer_than_80_chars
+          '${dateTime.toString().substring(0, dateTime.toString().length - 7)} | $message',
+    ),
+  );
+}
