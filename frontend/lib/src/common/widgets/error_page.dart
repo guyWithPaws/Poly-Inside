f781f900fbb5f113f3ab800288744e8b4da116be
goxxxa <g.o.molodtsov@gmail.com>
2025-03-14 00:54:16 +0300

    refactor: Applied color theme and fonts to error page

--- frontend/lib/src/common/widgets/error_page.dart
+++ frontend/lib/src/common/widgets/error_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meta/meta.dart';
+import 'package:poly_inside/src/common/theme.dart';
 
 /// {@template error_page}
 /// ErrorPage widget.
@@ -18,7 +20,8 @@ class ErrorPage extends StatefulWidget {
   /// that encloses the given context, if any.
   @internal
   // ignore: library_private_types_in_public_api
-  static _ErrorPageState? maybeOf(BuildContext context) => context.findAncestorStateOfType<_ErrorPageState>();
+  static _ErrorPageState? maybeOf(BuildContext context) =>
+      context.findAncestorStateOfType<_ErrorPageState>();
 
   @override
   State<ErrorPage> createState() => _ErrorPageState();
@@ -55,36 +58,48 @@ class _ErrorPageState extends State<ErrorPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            SvgPicture.asset('assets/icons/sad_face.svg'),
-            const SizedBox(
-              height: 16,
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(),
+        colorScheme: MaterialTheme.lightScheme().toColorScheme().copyWith(
+              surface: Colors.white,
+              onSurface: Colors.black,
+              outline: Colors.grey.shade700,
             ),
-            const Text('Упс... Кажется, что-то пошло не так'),
-            const Text('Попробуйте обновить страницу'),
-            const Spacer(),
-            Container(
-              width: 264,
-              height: 64,
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), color: Colors.green),
-              child: TextButton(
-                onPressed: () {},
-                child: Center(
-                  child: Text(
-                    'Обновить',
-                    style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                          color: Colors.white,
-                        ),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              SvgPicture.asset('assets/icons/sad_face.svg'),
+              const SizedBox(
+                height: 16,
+              ),
+              const Text('Упс... Кажется, что-то пошло не так'),
+              const Text('Попробуйте обновить страницу'),
+              const Spacer(),
+              Container(
+                width: 264,
+                height: 64,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.green),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Center(
+                    child: Text(
+                      'Обновить',
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 32)
-          ],
+              const SizedBox(height: 32)
+            ],
+          ),
         ),
       ),
     );
