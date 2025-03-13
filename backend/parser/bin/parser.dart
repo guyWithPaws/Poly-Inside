7fd15d2a18b832e7689b24bdbd1b02b9f3edc4b4
goxxxa <g.o.molodtsov@gmail.com>
2025-03-14 00:52:18 +0300

    refactor: Fixed database provider initialization

--- backend/parser/bin/parser.dart
+++ backend/parser/bin/parser.dart
@@ -10,9 +10,7 @@ Future<void> main() async {
       await runZonedGuarded(
         () async {
           final database = AppDatabase();
-
-          await DatabaseProviderImpl.initialize(database);
-          final provider = DatabaseProviderImpl();
+          final provider = DatabaseProviderImpl(database: database);
 
           await Future.delayed(const Duration(seconds: 10));
 
