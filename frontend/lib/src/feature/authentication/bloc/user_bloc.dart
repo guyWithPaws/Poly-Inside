2f9f38a4470f693c7e5f51ce5cd6f831728d0d4b
goxxxa <g.o.molodtsov@gmail.com>
2025-03-25 20:10:00 +0300

    refactor: Removed user parametrs logic, added warning message handler and ability to get a username from telegram

--- frontend/lib/src/feature/authentication/bloc/user_bloc.dart
+++ frontend/lib/src/feature/authentication/bloc/user_bloc.dart
@@ -5,44 +5,38 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:poly_inside/src/common/repository/client.dart';
 import 'package:shared/shared.dart';
-import 'package:flutter_telegram_web_app/flutter_telegram_web_app.dart' as tg;
+import 'package:telegram_web_app/telegram_web_app.dart';
+import 'package:poly_inside/src/feature/authentication/model/client_stub.dart'
+    if (dart.library.js_interop) 'package:poly_inside/src/feature/authentication/model/client_web.dart'
+    if (dart.library.io) 'package:poly_inside/src/feature/authentication/model/client_io.dart';
 
 part 'user_bloc.freezed.dart';
 
-int? getId() => 1234567890;
-String? getGroup() => '5132704/30003';
-
 class UserBloc extends Bloc<UserEvent, UserState> {
-  UserBloc({required final UserState state, required this.repository})
+  UserBloc(
+      {required final UserState state,
+      required this.repository})
       : super(state) {
-    on<GetUserEvent>((e, emit) async {
+    on<GetUserEvent>((event, emit) async {
       emit(const UserState.processing('Start logging in'));
-
-      var id = 1234567;
-      var name = 'developer';
-
-      try {
-        id = tg.initDataUnsafe.user!.id;
-        name = tg.initDataUnsafe.user!.first_name;
-      } catch (e) {}
 
       try {
         final id = getId();
-        final group = getGroup();
-        if (id == null || group == null) {
-          throw Exception('User ID is null');
-        }
-        emit(const UserState.processing('Getting user from database'));
+        emit(const UserState.processing(
+            'Getting user from database'));
 
         var user = await repository.getUserByUserId(id);
         if (!user.hasId()) {
           emit(const NotAuthorizedState());
         } else {
-          await FirebaseAnalytics.instance
-              .logLogin(parameters: <String, Object>{'UserID': user.id});
+          await FirebaseAnalytics.instance.logLogin(
+              parameters: <String, Object>{
+                'UserID': user.id
+              });
           emit(const UserState.processing('Loaded user'));
           emit(UserState.loaded(user));
         }
@@ -52,28 +46,47 @@ class UserBloc extends Bloc<UserEvent, UserState> {
         rethrow;
       }
     });
-    on<AuthenticationRequested>((e, emit) async {
+    on<AuthenticationRequested>((event, emit) async {
       try {
-        final user = User(id: getId(), name: e.name, group: getGroup());
-        emit(const UserState.processing('Adding user to database'));
+        var warningMessage = '';
+        if (event.name.isEmpty) {
+          warningMessage = 'Заполните поле с именем!';
+        } else if (!event.isLicenseAccepted) {
+          warningMessage =
+              'Примите пользовательское соглашение!';
+        }
 
-        await repository.addUser(user);
-        emit(const UserState.processing(
-            'Getting user from database after logging'));
+        if (warningMessage.isNotEmpty) {
+          emit(UserState.warning(warningMessage));
+        } else {
+          final user = User(
+              id: getId(),
+              name: event.name,
+              group: event.group);
+          emit(const UserState.processing(
+              'Adding user to database'));
 
-        await FirebaseAnalytics.instance
-            .logLogin(parameters: <String, Object>{'UserID': user.id});
-        emit(const UserState.processing('Loaded user'));
-        emit(UserState.loaded(user));
+          await repository.addUser(user);
+          emit(const UserState.processing(
+              'Getting user from database after logging'));
+
+          await FirebaseAnalytics.instance.logLogin(
+              parameters: <String, Object>{
+                'UserID': user.id
+              });
+          emit(const UserState.processing('Loaded user'));
+          emit(UserState.loaded(user));
+        }
       } catch (e) {
         rethrow;
       }
     });
-    on<GroupsList>((e, emit) => emit(UserState.groupLoaded(e.groups)));
-    on<GroupTextFieldChanged>((e, emit) async {
+    on<GroupsList>((event, emit) =>
+        emit(UserState.groupLoaded(event.groups)));
+    on<GroupTextFieldChanged>((event, emit) async {
       try {
         _subscription?.cancel();
-        final list = repository.findGroup(5, e.group);
+        final list = repository.findGroup(5, event.group);
         _subscription = list.listen(
           (data) {
             add(GroupsList(groups: data.groups));
@@ -82,6 +95,20 @@ class UserBloc extends Bloc<UserEvent, UserState> {
       } catch (e) {
         rethrow;
       }
+    });
+    on<TelegramUserNameRequest>((event, emit) {
+      try {
+        var user = TelegramWebApp.instance.initData.user;
+        event._controller.text = user.firstname ??
+            user.username ??
+            user.lastname ??
+            'dev';
+      } catch (e) {
+        rethrow;
+      }
+    });
+    on<ResetWarningEvent>((event, emit) {
+      emit(const UserState.notAuthorized());
     });
   }
   final ClientRepository repository;
@@ -92,15 +119,31 @@ abstract class UserEvent {
   const UserEvent();
 }
 
-class Request extends UserEvent {}
-
 class GetUserEvent extends UserEvent {
   GetUserEvent();
 }
 
+class ResetWarningEvent extends UserEvent {}
+
+class AuthenticationRequestedWithoutGroup
+    extends UserEvent {}
+
+class TelegramUserNameRequest extends UserEvent {
+  final TextEditingController _controller;
+
+  TelegramUserNameRequest(
+      {required TextEditingController controller})
+      : _controller = controller;
+}
+
 class AuthenticationRequested extends UserEvent {
   final String name;
-  const AuthenticationRequested({required this.name});
+  final String group;
+  final bool isLicenseAccepted;
+  const AuthenticationRequested(
+      {required this.group,
+      required this.name,
+      required this.isLicenseAccepted});
 }
 
 class GroupTextFieldChanged extends UserEvent {
@@ -115,13 +158,22 @@ class GroupsList extends UserEvent {
   GroupsList({required this.groups});
 }
 
+class SeeUserLicenseRequest extends UserEvent {}
+
 @Freezed()
 sealed class UserState with _$UserState {
   const UserState._();
-  const factory UserState.processing(String stage) = ProcessingState;
+  const factory UserState.processing(String stage) =
+      ProcessingState;
   const factory UserState.idle() = IdleState;
-  const factory UserState.notAuthorized() = NotAuthorizedState;
+  const factory UserState.notAuthorized() =
+      NotAuthorizedState;
   const factory UserState.error(Object e) = ErrorState;
   const factory UserState.loaded(User user) = LoadedState;
-  const factory UserState.groupLoaded(List<GroupNumber> groups) = GroupsLoadedState;
+  const factory UserState.nameLoaded(String name) =
+      NameLoadedState;
+  const factory UserState.warning(String message) =
+      WarningState;
+  const factory UserState.groupLoaded(
+      List<GroupNumber> groups) = GroupsLoadedState;
 }
