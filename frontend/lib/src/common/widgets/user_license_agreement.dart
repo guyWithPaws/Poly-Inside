329b4b2043c44b3cc5f0fb511c64c0653cb42091
goxxxa <g.o.molodtsov@gmail.com>
2025-03-25 16:27:32 +0300

    refactor: Migrated to StatelessWidget to improve perfomance

--- frontend/lib/src/common/widgets/user_license_agreement.dart
+++ frontend/lib/src/common/widgets/user_license_agreement.dart
@@ -2,21 +2,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../../feature/authentication/bloc/user_bloc.dart';
 import '../theme.dart';
 
-class UserLicenseAgreement extends StatefulWidget {
-  const UserLicenseAgreement({super.key, required this.bloc});
+class UserLicenseAgreement extends StatelessWidget {
+  const UserLicenseAgreement({super.key});
 
-  final UserBloc? bloc;
-
-  @override
-  State<UserLicenseAgreement> createState() => _UserLicenseAgreementState();
-}
-
-class _UserLicenseAgreementState extends State<UserLicenseAgreement> {
   @override
   Widget build(BuildContext context) {
+    Widget buildSectionTitle(String title) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.black87,
+          ),
+        ),
+      );
+    }
+
+    Widget buildSectionContent(String content) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: Text(
+          content,
+          style: const TextStyle(
+            fontSize: 16.0,
+            color: Colors.black54,
+            height: 1.5,
+          ),
+        ),
+      );
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -50,103 +70,74 @@ class _UserLicenseAgreementState extends State<UserLicenseAgreement> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildSectionTitle('1. ОБЩИЕ ПОЛОЖЕНИЯ'),
-              _buildSectionContent(
+              buildSectionTitle('1. ОБЩИЕ ПОЛОЖЕНИЯ'),
+              buildSectionContent(
                 '1.1. Настоящее Пользовательское соглашение (далее — «Соглашение») регулирует права и обязанности Пользователя (далее — «Пользователь») и администратора приложения (далее — «Приложение», «Мы») в отношении использования Приложения для оценки преподавателей.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '1.2. Используя Приложение, Пользователь подтверждает, что ознакомлен с условиями настоящего Соглашения, принимает его и обязуется соблюдать все положения, изложенные ниже. Если Пользователь не согласен с условиями, он не имеет права использовать Приложение.',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle('2. ОТВЕТСТВЕННОСТЬ'),
-              _buildSectionContent(
+              buildSectionTitle('2. ОТВЕТСТВЕННОСТЬ'),
+              buildSectionContent(
                 '2.1. Мы не несём ответственности за точность, полноту и достоверность информации, оставляемой Пользователями в Приложении, включая оценки и комментарии о преподавателях. Приложение предоставляет исключительно платформу для обмена мнениями и не гарантирует, что информация будет объективной или правдивой.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '2.2. Мы не несем ответственности за последствия, возникающие из-за оставленных Пользователями негативных комментариев или оценок, включая возможные последствия для репутации преподавателей или третьих лиц.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '2.3. Приложение не несет ответственности за возможные ошибки или сбои, возникшие в процессе использования Приложения, включая технические неполадки, которые могут повлиять на работу системы и сохранение данных.',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle('3. ОБЯЗАННОСТИ ПОЛЬЗОВАТЕЛЕЙ'),
-              _buildSectionContent(
+              buildSectionTitle('3. ОБЯЗАННОСТИ ПОЛЬЗОВАТЕЛЕЙ'),
+              buildSectionContent(
                 '3.1. Пользователи обязаны предоставлять только достоверную и актуальную информацию о преподавателях.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '3.2. Запрещается размещать в Приложении информацию, которая:\n- Нарушает права и свободы других пользователей, преподавателей или третьих лиц.\n- Является клеветой, угрозами или нарушает моральные и этические нормы.\n- Нарушает законодательство Российской Федерации, в том числе, но не ограничиваясь, законами, регулирующими защиту чести, достоинства и деловой репутации.\n- Включает оскорбления, расистские высказывания, угрозы насилия или дискриминацию.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '3.3. Пользователи соглашаются не использовать Приложение для размещения ложных данных, манипуляции с оценками и комментариями, а также для других недобросовестных действий.',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle('4. КОНФИДЕНЦИАЛЬНОСТЬ И ПЕРСОНАЛЬНЫЕ ДАННЫЕ'),
-              _buildSectionContent(
+              buildSectionTitle('4. КОНФИДЕНЦИАЛЬНОСТЬ И ПЕРСОНАЛЬНЫЕ ДАННЫЕ'),
+              buildSectionContent(
                 '4.1. Мы обязуемся соблюдать конфиденциальность персональных данных, предоставленных Пользователями в процессе использования Приложения. Персональные данные будут обрабатываться в соответствии с действующим законодательством о защите персональных данных.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '4.2. Пользователь соглашается с тем, что его личные данные могут быть использованы для регистрации и функционирования Приложения, а также для отправки уведомлений и маркетинговых материалов (если Пользователь не отказался от получения таких материалов).',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle('5. ПРАВА И ОБЯЗАННОСТИ АДМИНИСТРАТОРА'),
-              _buildSectionContent(
+              buildSectionTitle('5. ПРАВА И ОБЯЗАННОСТИ АДМИНИСТРАТОРА'),
+              buildSectionContent(
                 '5.1. Мы оставляем за собой право в любое время изменять или приостанавливать работу Приложения, а также модерацию контента, размещаемого Пользователями.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '5.2. Мы имеем право удалять комментарии и оценки, нарушающие настоящее Соглашение, законодательство РФ, а также правила и стандарты Приложения.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '5.3. Мы не обязаны контролировать каждое отдельное мнение, размещённое Пользователями, но при наличии жалоб или сигналов о нарушениях мы можем провести проверку и принять соответствующие меры.',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle('6. ИЗМЕНЕНИЯ В СОГЛАШЕНИИ'),
-              _buildSectionContent(
+              buildSectionTitle('6. ИЗМЕНЕНИЯ В СОГЛАШЕНИИ'),
+              buildSectionContent(
                 '6.1. Мы оставляем за собой право в любое время изменять условия настоящего Соглашения без предварительного согласования с Пользователем. Изменения вступают в силу с момента их опубликования в Приложении.',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle(
-                  '7. ПРАВО ИСКЛЮЧИТЕЛЬНОЙ ПРИОРИТЕТНОЙ СТАТУСА'),
-              _buildSectionContent(
+              buildSectionTitle('7. ПРАВО ИСКЛЮЧИТЕЛЬНОЙ ПРИОРИТЕТНОЙ СТАТУСА'),
+              buildSectionContent(
                 '7.1. В случае возникновения споров, связанных с использованием Приложения, стороны будут пытаться разрешить их путём переговоров. Если сторонам не удастся достичь соглашения, спор будет передан в суд по месту нахождения Ответчика.',
               ),
               const SizedBox(height: 16.0),
-              _buildSectionTitle('8. ПРОЧИЕ УСЛОВИЯ'),
-              _buildSectionContent(
+              buildSectionTitle('8. ПРОЧИЕ УСЛОВИЯ'),
+              buildSectionContent(
                 '8.1. Все вопросы, не урегулированные данным Соглашением, регулируются законодательством Российской Федерации.',
               ),
-              _buildSectionContent(
+              buildSectionContent(
                 '8.2. Если какое-либо положение настоящего Соглашения будет признано недействительным, это не повлияет на действительность остальных положений.',
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSectionTitle(String title) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16.0),
-      child: Text(
-        title,
-        style: const TextStyle(
-          fontSize: 18.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.black87,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSectionContent(String content) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0),
-      child: Text(
-        content,
-        style: const TextStyle(
-          fontSize: 16.0,
-          color: Colors.black54,
-          height: 1.5,
         ),
       ),
     );
