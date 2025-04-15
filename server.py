6fcf3aae307d85672f51ead2f8bf3555cef5e7d8
goxxxa <g.o.molodtsov@gmail.com>
2025-04-15 14:23:56 +0300

    refactor: Deleted deploy folder, its functionality will be replaced with github actions.

--- server.py
+++
@@ -1,7 +0,0 @@
-from deploy.server_builder import ServerBuilder 
-import logging
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, filename='deploy/server.log', filemode='w')
-    server = ServerBuilder()
-    server.run()
