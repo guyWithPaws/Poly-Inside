1308e176a2d693b18d8ade2dff9d5dcbce022bb1
goxxxa <g.o.molodtsov@gmail.com>
2025-05-16 13:47:35 +0300

    refactor: Fixed problem with user's photo preview

--- frontend/lib/src/common/widgets/review_title.dart
+++ frontend/lib/src/common/widgets/review_title.dart
@@ -1,16 +1,20 @@
 import 'dart:typed_data';
 
+import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:poly_inside/src/common/extensions/string.dart';
 import 'package:poly_inside/src/common/widgets/professor_features.dart';
-import 'package:poly_inside/src/feature/initialization/widget/initialization.dart';
 import 'package:poly_inside/src/feature/review/widget/review_page.dart';
 import 'package:shared/shared.dart';
 import 'package:intl/intl.dart';
 
 import '../../feature/authentication/widget/user_scope.dart';
+import '../../feature/user_profile/bloc/likes_bloc/likes_bloc.dart';
+import '../../feature/user_profile/bloc/user_bloc/user_bloc.dart';
+import '../enums/reaction_type.dart';
 import '../enums/review_type.dart';
 
 /// {@template review_title}
@@ -45,48 +49,48 @@ class ReviewTitle extends StatelessWidget {
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Uint8List.fromList(professor!.avatar).isNotEmpty
-                          ? Image.memory(
-                              Uint8List.fromList(professor!.avatar),
-                              width: 60,
-                              height: 60,
-                              cacheHeight: 160,
-                              cacheWidth: 160,
-                            )
-                          : SvgPicture.asset('assets/icons/no_photo.svg'),
-                      // CircleAvatar(
-                      //   backgroundColor: Colors.grey[200],
-                      //   radius: 20,
-                      //   child: ClipOval(
-                      //     child: user == null
-                      //         ? professor!.smallAvatar.isNotEmpty
-                      //             ? Image.memory(
-                      //                 height: 60,
-                      //                 width: 60,
-                      //                 fit: BoxFit.cover,
-                      //                 Uint8List.fromList(
-                      //                   professor!.smallAvatar,
-                      //                 ),
-                      //               )
-                      //             : SvgPicture.asset(
-                      //                 'assets/icons/no_photo.svg',
-                      //                 width: 30,
-                      //               )
-                      //         : user!.avatar.isNotEmpty
-                      //             ? Image.memory(
-                      //                 height: 60,
-                      //                 width: 60,
-                      //                 fit: BoxFit.cover,
-                      //                 Uint8List.fromList(
-                      //                   user!.avatar,
-                      //                 ),
-                      //               )
-                      //             : SvgPicture.asset(
-                      //                 'assets/icons/no_photo.svg',
-                      //                 width: 30,
-                      //               ),
-                      //   ),
-                      // ),
+                      // Uint8List.fromList(professor!.avatar).isNotEmpty
+                      //     ? Image.memory(
+                      //         Uint8List.fromList(professor!.avatar),
+                      //         width: 60,
+                      //         height: 60,
+                      //         cacheHeight: 160,
+                      //         cacheWidth: 160,
+                      //       )
+                      //     : SvgPicture.asset('assets/icons/no_photo.svg'),
+                      CircleAvatar(
+                        backgroundColor: Colors.grey[200],
+                        radius: 20,
+                        child: ClipOval(
+                          child: user == null
+                              ? professor!.smallAvatar.isNotEmpty
+                                  ? Image.memory(
+                                      height: 60,
+                                      width: 60,
+                                      fit: BoxFit.cover,
+                                      Uint8List.fromList(
+                                        user!.avatar,
+                                      ),
+                                    )
+                                  : SvgPicture.asset(
+                                      'assets/icons/no_photo.svg',
+                                      width: 30,
+                                    )
+                              : user!.avatar.isNotEmpty
+                                  ? Image.memory(
+                                      height: 60,
+                                      width: 60,
+                                      fit: BoxFit.cover,
+                                      Uint8List.fromList(
+                                        user!.avatar,
+                                      ),
+                                    )
+                                  : SvgPicture.asset(
+                                      'assets/icons/no_photo.svg',
+                                      width: 30,
+                                    ),
+                        ),
+                      ),
                       const SizedBox(width: 16),
                       Expanded(
                         child: Text(
@@ -169,22 +173,22 @@ class ReviewTitle extends StatelessWidget {
                                                       children: [
                                                         TextButton(
                                                           onPressed: () async {
-                                                            await InitializationScope
-                                                                    .repositoryOf(
-                                                                        context)
-                                                                .deleteReview(
-                                                                    review.id)
-                                                                .then(
-                                                              (_) {
-                                                                if (context
-                                                                    .mounted) {
-                                                                  Navigator.pop(
-                                                                      context);
-                                                                  Navigator.pop(
-                                                                      context);
-                                                                }
-                                                              },
+                                                            BlocProvider.of<
+                                                                        ProfileDataBLoC>(
+                                                                    context)
+                                                                .add(
+                                                              DeleteReview(
+                                                                  reviewId:
+                                                                      review
+                                                                          .id),
                                                             );
+                                                            if (context
+                                                                .mounted) {
+                                                              Navigator.pop(
+                                                                  context);
+                                                              Navigator.pop(
+                                                                  context);
+                                                            }
                                                           },
                                                           child:
                                                               const Text('Да'),
@@ -258,60 +262,73 @@ class ReviewTitle extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                    DateFormat.yMMMMd('ru_RU').format(
-                        DateFormat('yyyy-MM-dd HH:mm:ss.SSSSSS')
-                            .parse(review.date)),
-                    style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w500)),
-                // SizedBox(
-                //   child: Row(
-                //     children: [
-                //       GestureDetector(
-                //         onTap: () => BlocProvider.of<LikesBLoC>(context).add(
-                //           AddLike(
-                //               reviewId: review.id, professorId: professor!.id),
-                //         ),
-                //         child: SvgPicture.asset(
-                //           ReactionType.like.path,
-                //           alignment: Alignment.bottomRight,
-                //           colorFilter: const ColorFilter.mode(
-                //               Color.fromARGB(255, 138, 138, 138),
-                //               BlendMode.srcIn),
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         width: 8,
-                //       ),
-                //       Text('${review.likes}'),
-                //       const SizedBox(width: 16),
-                //       GestureDetector(
-                //         onTap: () => BlocProvider.of<LikesBLoC>(context).add(
-                //           AddDislike(reviewId: review.id),
-                //         ),
-                //         child: SvgPicture.asset(
-                //           ReactionType.dislike.path,
-                //           alignment: Alignment.bottomRight,
-                //           colorFilter: const ColorFilter.mode(
-                //               Color.fromARGB(255, 138, 138, 138),
-                //               BlendMode.srcIn),
-                //         ),
-                //       ),
-                //       const SizedBox(
-                //         width: 8,
-                //       ),
-                //       AnimatedFlipCounter(
-                //         value: review.dislikes,
-                //         duration: const Duration(milliseconds: 200),
-                //         textStyle: const TextStyle(
-                //           color: Color.fromARGB(255, 138, 138, 138),
-                //           fontWeight: FontWeight.w400,
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // )
+                  DateFormat.yMMMMd('ru_RU').format(
+                      DateFormat('yyyy-MM-dd HH:mm:ss.SSSSSS')
+                          .parse(review.date)),
+                  style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () => BlocProvider.of<LikesBLoC>(context).add(
+                          AddLike(
+                              review: review,
+                              reviewId: review.id,
+                              professorId: professor!.id),
+                        ),
+                        child: SvgPicture.asset(
+                          ReactionType.like.path,
+                          alignment: Alignment.bottomRight,
+                          colorFilter: ColorFilter.mode(
+                              (review.currentUserReaction == 2)
+                                  ? const Color.fromARGB(255, 26, 255, 0)
+                                  : const Color.fromARGB(255, 138, 138, 138),
+                              BlendMode.srcIn),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      AnimatedFlipCounter(
+                        value: review.likes,
+                        duration: const Duration(milliseconds: 200),
+                        textStyle: const TextStyle(
+                          color: Color.fromARGB(255, 138, 138, 138),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      GestureDetector(
+                        onTap: () => BlocProvider.of<LikesBLoC>(context).add(
+                          AddDislike(
+                              reviewId: review.id, professorId: professor!.id),
+                        ),
+                        child: SvgPicture.asset(
+                          ReactionType.dislike.path,
+                          alignment: Alignment.bottomRight,
+                          colorFilter: const ColorFilter.mode(
+                              Color.fromARGB(255, 138, 138, 138),
+                              BlendMode.srcIn),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      AnimatedFlipCounter(
+                        value: review.dislikes,
+                        duration: const Duration(milliseconds: 200),
+                        textStyle: const TextStyle(
+                          color: Color.fromARGB(255, 138, 138, 138),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
                 // Reactions(
                 //   review: review,
                 //   reaction: reaction,
